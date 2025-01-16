505f06900b371d2e3829ce8c2665ab2f6dfa6213
Carlos Taborda <carlos_taborda@info-design.co>
2025-01-16 07:22:43 -0500

    documentation

--- Client/src/pages/LIstOfCommissionAdmin.tsx
+++ Client/src/pages/LIstOfCommissionAdmin.tsx
@@ -1,32 +1,43 @@
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Container,
-  InputAdornment,
-  Menu,
-  MenuItem,
-  Paper,
-  TextField,
-  Typography,
-} from "@mui/material";
-// import TableUI from "../components/TableUI/TableUI";
-// import { GridColDef } from "@mui/x-data-grid";
+/**
+ * @author Carlos Taborda
+ * @description This component is the loader to display the administrator commissions and reserve them per day.
+ * @version 1.0
+ *
+ */
 
-import { useSales } from "../hooks/useSales";
+import { useEffect, useState, ChangeEvent, MouseEvent } from "react";
+
+/**Libreries */
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import Container from "@mui/material/Container";
+import InputAdornment from "@mui/material/InputAdornment";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 import { format } from "@formkit/tempo";
 import { Icon } from "@iconify/react";
 
+/**Hooks */
+import { useSales } from "../hooks/useSales";
+
+/**Component */
 import DialogListAccompanist from "../components/Dialogs/DialogListAccompanist";
 
+/**Functions */
 import { formatPrice } from "../generalFunctions/formaters";
 
+/**Icon */
 import searchIcon from "../../src/assets/searchIcon.svg";
 
-import { IGetListSales } from "../interfaces/IUser";
-import { useEffect, useState, ChangeEvent, MouseEvent } from "react";
+/**Interface */
+import { IGetListSales, IAccompanistListSales } from "../interfaces/IUser";
 
-import { IAccompanistListSales } from "../interfaces/IUser";
+// import TableUI from "../components/TableUI/TableUI";
+// import { GridColDef } from "@mui/x-data-grid";
 
 const FORMAT_DATE = "YYYY/MM/DD";
 
@@ -123,17 +134,6 @@ const LIstOfCommissionAdmin = () => {
     setAnchorEl(null);
   };
 
-  const handleFilterCode = () => {
-    const searchInput = document.getElementById(
-      "searchInput"
-    ) as HTMLInputElement;
-    const value = searchInput.value;
-    const filter = dataList.filter((item) =>
-      item.code_reservation.includes(value.toUpperCase())
-    );
-    setDataListFilter(filter);
-  };
-
   const handleOnchange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     const filter = dataList.filter((item) =>
@@ -207,31 +207,6 @@ const LIstOfCommissionAdmin = () => {
               onChange={handleOnchange}
             />
             <img src={searchIcon} style={{ paddingInlineEnd: "18px" }} />
-          </Box>
-
-          <Box
-            sx={{
-              backgroundColor: "#2B3D5E",
-              height: 40,
-              width: 100,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: 100,
-              gap: 10,
-            }}
-          >
-            <Button
-              size="small"
-              sx={{
-                textTransform: "none",
-                fontSize: 14,
-                color: "#FFFFFF",
-              }}
-              onClick={handleFilterCode}
-            >
-              Buscar
-            </Button>
           </Box>
         </Box>
 
