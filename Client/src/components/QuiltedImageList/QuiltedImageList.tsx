3e0e44f0eea02e3032197c2a1275eb8151ea39b3
Carlos Taborda <solrac1907@live.com>
2025-02-02 14:52:25 -0500

    images

--- Client/src/components/QuiltedImageList/QuiltedImageList.tsx
+++ Client/src/components/QuiltedImageList/QuiltedImageList.tsx
@@ -22,11 +22,16 @@ export default function QuiltedImageList() {
     >
       {itemData.map((item) => (
         <ImageListItem
+          style={{ position: "relative" }}
           key={item.img}
           cols={item.cols || 1}
           rows={item.rows || 1}
         >
-          <LazyImage {...srcset(item.img, 121, item.rows, item.cols)} />
+          <LazyImage
+            {...srcset(item.img, 121, item.rows, item.cols)}
+            title={item.title}
+            titleEN={item.titleEN}
+          />
         </ImageListItem>
       ))}
     </ImageList>
@@ -35,39 +40,52 @@ export default function QuiltedImageList() {
 
 const itemData = [
   {
-    img: "/images/MangataFire.jpg",
-    title: "Fire",
-    rows: 2,
+    img: "/images/lunche/filete de pescado apanado.jpg",
+    title: "Filete de pescado apanado",
+    titleEN: "Breadded fish fillet",
+    rows: 4,
     cols: 2,
   },
   {
-    img: "/images/Mangata1.jpg",
-    title: "Langosta",
+    img: "/images/lunche/nuggets de pollo.jpg",
+    title: "Nuggets de pollo",
+    titleEN: "Chicken nuggets",
     cols: 2,
-    rows: 2,
+    rows: 7,
   },
   {
-    img: "/images/Mangata6.jpg",
-    title: "Coffee",
-    rows: 2,
+    img: "/images/lunche/pasta boloñesa.jpg",
+    title: "Pasta boloñesa",
+    titleEN: "Bolonese pasta",
+    rows: 4,
     cols: 2,
   },
   {
-    img: "/images/Mangata2.jpg",
-    title: "Camera",
-    rows: 2,
+    img: "/images/lunche/pasta vegetariana.jpg",
+    title: "Pasta vegetariana",
+    titleEN: "Vegetarian pasta",
+    rows: 4,
     cols: 2,
   },
 
   {
-    img: "/images/Mangata4.jpg",
-    title: "Hats",
-    rows: 2,
+    img: "/images/lunche/pescado frito.jpg",
+    title: "Pescado frito",
+    titleEN: "Fried fish",
+    rows: 4,
     cols: 2,
   },
   {
-    img: "/images/Mangata7.jpg",
-    title: "Honey",
+    img: "/images/lunche/risoto.jpg",
+    title: "Risoto",
+    titleEN: "Risoto",
+    rows: 3,
+    cols: 2,
+  },
+  {
+    img: "/images/lunche/pizza1.jpg",
+    title: "Pizza",
+    titleEN: "Pizza",
     rows: 2,
     cols: 2,
   },
