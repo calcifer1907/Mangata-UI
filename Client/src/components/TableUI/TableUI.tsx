172a5649936606e169c8156763956afecc00c2eb
Carlos Taborda <solrac1907@live.com>
2025-03-09 13:20:43 -0500

    change date

--- Client/src/components/TableUI/TableUI.tsx
+++ Client/src/components/TableUI/TableUI.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Paper } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { FC } from "react";
