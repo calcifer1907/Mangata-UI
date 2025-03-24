b7ea9ca42255e1cac831e8ee9bc2784cf41c1b4a
Carlos Taborda <solrac1907@live.com>
2025-03-24 08:25:05 -0500

    refactor code server (user), add to repository

--- Server/src/controllers/user.controller.ts
+++ Server/src/controllers/user.controller.ts
@@ -2,95 +2,51 @@ import { Request, Response } from "express";
 
 import { pool } from "../Connection";
 
-export const getListUSers = async (
-  _request: Request,
-  response: Response
-): Promise<Response<any, Record<string, any>> | undefined> => {
-  try {
-    const result = await pool.query("SELECT * FROM users;");
-    response.json(result.rows);
-  } catch (error) {
-    return response.status(500).json({ message: "sometghin gos wrong" });
-  }
-};
+import UserRepository from "../repository/userRepository";
+import AppError from "../errors/appError";
 
-export const getSearchUser = async (
-  request: Request,
-  response: Response
-): Promise<Response<any, Record<string, any>> | undefined> => {
-  try {
-    const { id } = request.body;
-    const result = await pool.query(
-      "SELECT CONCAT(FIRST_NAME,' ',LAST_NAME) AS USER_NAME,ID FROM users WHERE ID=$1;",
-      [id]
-    );
-    if (result.rows.length === 0) {
-      const system = await pool.query(
-        "SELECT CONCAT(FIRST_NAME,' ',LAST_NAME) AS USER_NAME,ID FROM users WHERE CONCAT(FIRST_NAME,LAST_NAME) LIKE '%system%';"
-      );
-      return response.json(system.rows[0]);
+class UserController {
+  async getUserName(request: Request, response: Response): Promise<void> {
+    try {
+      const { id } = request.body;
+      if (id !== -1) {
+        const result = await UserRepository.getUserName(id);
+        response.json(result);
+      } else {
+        const system = await UserRepository.getUserNameSystem();
+        response.json(system);
+      }
+    } catch (error) {
+      if (error instanceof AppError) {
+        response.status(error.statusCode).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: "Something wrong error!" });
+      }
     }
-    response.json(result.rows[0]);
-  } catch (error) {
-    return response.status(500).json({ message: "sometghin gos wrong" });
   }
-};
 
-export const createUser = async (request: Request, response: Response) => {
-  const {
-    FIRST_NAME,
-    LAST_NAME,
-    EMAIL,
-    BANK_ACCOUNT,
-    ROLE_ID,
-    PASSWORD,
-    IS_ACTIVE,
-    CREATED_AT,
-  } = request.body;
-
-  try {
-    const values = [
-      FIRST_NAME,
-      LAST_NAME,
-      EMAIL,
-      BANK_ACCOUNT,
-      PASSWORD,
-      ROLE_ID,
-      IS_ACTIVE,
-      CREATED_AT,
-    ];
-    const result = await pool.query(
-      "INSERT INTO users(FIRST_NAME,LAST_NAME,EMAIL,BANK_ACCOUNT,PASSWORD,ROLE_ID,IS_ACTIVE,CREATED_AT) VALUES($1,$2,$3,$4,$5,$6,$7,$8);",
-      values
-    );
-    if (result.rowCount === 0) throw new Error("Error al insertar");
-    response.json({ message: "success" });
-  } catch (error) {
-    if ((error as { code: string })?.code === "23505")
-      return response
-        .status(409)
-        .json({ message: "Error: El correo electrónico ya está registrado." });
-
-    return response.status(500).json({ message: "sometghin gos wrong" });
+  async createUser(request: Request, response: Response) {
+    try {
+      const values = [request.body];
+      const result = await UserRepository.createUser(values);
+      response.json(result);
+    } catch (error) {
+      if ((error as { code: string })?.code === "23505")
+        response.status(409).json({
+          message: "Error: El correo electrónico ya está registrado.",
+        });
+    }
   }
-};
 
-export const validExistEmail = async (
-  request: Request,
-  response: Response
-): Promise<Response> => {
-  const { email } = request.body;
-  try {
-    const result = await pool.query("SELECT EMAIL FROM users WHERE EMAIL=$1;", [
-      email,
-    ]);
-    if (result.rows.length > 0)
-      return response.json({
-        message: "El correo ya está registrado",
-        status: 409,
-      });
-    return response.json({ message: "Correo disponible", status: 201 });
-  } catch (error) {
-    return response.status(500).json({ message: "sometghin gos wrong" });
+  async validExistEmail(request: Request, response: Response): Promise<void> {
+    const { email } = request.body;
+    try {
+      const result = await UserRepository.validExistEmail(email);
+      response.json(result);
+    } catch (error) {
+      response.status(500).json({ message: "sometghin gos wrong" });
+    }
   }
-};
+}
+
+export default new UserController();
---
+++ Server/src/interfaces/IUSers.ts
@@ -0,0 +1,19 @@
+export interface IUserName {
+  user_name: string;
+  id: number;
+}
+
+export interface IMessages {
+  message: string;
+}
+
+export interface ISaveUSer {
+  FIRST_NAME: string;
+  LAST_NAME: string;
+  EMAIL: string;
+  BANK_ACCOUNT: string;
+  PASSWORD: string;
+  ROLE_ID: number;
+  IS_ACTIVE: boolean;
+  CREATED_AT: string;
+}
