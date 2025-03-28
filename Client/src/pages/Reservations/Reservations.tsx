d85b9fe35a19769efb90540001ca8403362beea1
Carlos Taborda <solrac1907@live.com>
2025-03-28 13:00:59 -0500

    valid email

--- Client/src/pages/Reservations/Reservations.tsx
+++ Client/src/pages/Reservations/Reservations.tsx
@@ -38,7 +38,6 @@ const NameLunchForm: React.FC = () => {
   const {
     optionsLunches,
     dateChange,
-
     errors,
     valueCel,
     valueEmail,
@@ -75,7 +74,6 @@ const NameLunchForm: React.FC = () => {
       document.getElementById("contentPrimary")?.offsetHeight;
     setMaxHeight(heightContainer || 0);
   }, []);
-
 
   const style = {
     position: "absolute",
@@ -185,6 +183,7 @@ const NameLunchForm: React.FC = () => {
               value={valueEmail}
               onChange={handleOnChangeEmail}
               label="Correo"
+              type="email"
               placeholder="Ingrese tú Correo"
               iconName="letter-opened"
               helperText="Este campo es obligatorio"
