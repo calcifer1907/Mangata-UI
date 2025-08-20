b9834159366829340b318186419088c498a25d2d
Carlos Taborda <tabordac2@gmail.com>
2025-08-20 11:42:05 -0500

    configuration optimization

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import imagemin from "vite-plugin-imagemin";
+import compression from "vite-plugin-compression";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +17,33 @@ export default defineConfig({
         },
       ],
     }),
+    imagemin({
+      gifsicle: { optimizationLevel: 3 },
+      mozjpeg: { quality: 80 },
+      pngquant: { quality: [0.8, 0.9] },
+      svgo: {
+        plugins: [{ removeViewBox: false }, { removeUnusedNS: false }],
+      },
+    }),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          utils: ["lodash", "axios"],
+        },
+      },
+    },
+  },
 });
