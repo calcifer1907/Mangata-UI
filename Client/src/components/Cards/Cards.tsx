ef4b2e496887c69263987aa539fbfb3f3e1f1577
Carlos Taborda <tabordac2@gmail.com>
2025-12-06 14:45:08 -0500

    refactor card

--- Client/src/components/Cards/Cards.tsx
+++ Client/src/components/Cards/Cards.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 interface IProsp {
   title: string;
@@ -11,12 +12,13 @@ interface IProsp {
 import "./styleCards.css";
 
 const Cards = ({ title, img, button, redirectTo, buttonTitle }: IProsp) => {
+  const { t } = useTranslation("home");
   return (
     <Box className="container-cards">
       <Box component="img" src={img} className="wd-100 image-card" />
       <Box className="content-card">
         <Box className="hg-100 d-flex justify-content-evenly align-items-center flex-direction-column">
-          <Typography className="font-weight-600 font-size-16 title-card">
+          <Typography className="font-weight-600 font-size-22 title-card">
             {title}
           </Typography>
           {button && (
@@ -25,7 +27,7 @@ const Cards = ({ title, img, button, redirectTo, buttonTitle }: IProsp) => {
               href={redirectTo}
               className="buttonDiscover"
             >
-              {buttonTitle}
+              {buttonTitle ? buttonTitle : t("discover")}
             </Typography>
           )}
         </Box>
