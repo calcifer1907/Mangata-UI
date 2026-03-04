39e7fe3b9dc99096f0fccf0a270cca2f8ade5898
Carlos Taborda <tabordac2@gmail.com>
2026-03-04 12:55:13 -0500

    add temple to email and update controller

--- Server/src/controllers/sendEmail.controller.ts
+++ Server/src/controllers/sendEmail.controller.ts
@@ -20,21 +20,65 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const boat = {
+  clientName: "",
+  clientEmail: "",
+  clientPhone: "+57 312 6056467",
+  startDate: "15/06/2024",
+  endDate: "18/06/2024",
+  duration: "3",
+  departurePort: "Muelle Todo Mar",
+  totalPrice: "",
+};
+
 export const sendEmail = async (payment_id: string) => {
   try {
     const subject = "Welcome to Mangata Beach Club";
     const QUERY =
-      "SELECT TO_CHAR(CREATED_AT AT TIME ZONE 'UTC', 'YYYY-MM-DD') AS FORMATTED_DATE, EMAIL FROM reservations WHERE PAYMENT_ID = $1";
+      "SELECT TO_CHAR(CREATED_AT AT TIME ZONE 'UTC', 'YYYY-MM-DD') AS FORMATTED_DATE, EMAIL,ID_EMPLOYEE, CURRENT_COMMISSION,CODE_RESERVATION FROM reservations WHERE PAYMENT_ID = $1";
     const { rows, rowCount } = await pool.query(QUERY, [payment_id]);
+
     if (rowCount) {
       const [resultQuery] = rows;
-      const plantillaPath = path.join("./src/html", "htmlTemplateDayPass.html");
+      const {
+        formatted_date,
+        id_employee,
+        email,
+        current_commission,
+        code_reservation,
+      } = resultQuery;
+      boat.clientEmail = email;
+      boat.startDate = formatted_date;
+      boat.endDate = formatted_date;
+      boat.totalPrice = new Intl.NumberFormat("es-CO", {
+        style: "currency",
+        currency: "COP",
+        minimumFractionDigits: 0, // Evitar mostrar los decimales
+        maximumFractionDigits: 0, // Evitar decimales adicionales
+      }).format(current_commission);
+
+      if (id_employee === "30") {
+        const QUERY_NAME_CLIENT =
+          "SELECT NAME_ACCOMPANIST FROM accompanist WHERE ID_RESERVATION = $1";
+        const { rows: rowsNameClient } = await pool.query(QUERY_NAME_CLIENT, [
+          code_reservation,
+        ]);
+        if (rowsNameClient.length > 0) {
+          boat.clientName = rowsNameClient[0].name_accompanist;
+        }
+      }
+
+      const TEMPLATE =
+        id_employee === "30"
+          ? "htmlTemplateReservationBoat.html"
+          : "htmlTemplateDayPass.html";
+
+      const plantillaPath = path.join("./src/html", TEMPLATE);
       let htmlTemplate = fs.readFileSync(plantillaPath, "utf8");
-      const dataUSer = { create_at: resultQuery.formatted_date };
-      htmlTemplate = replacePlaceholders(htmlTemplate, dataUSer);
+      htmlTemplate = replacePlaceholders(htmlTemplate, boat);
       const mailOptions = {
         from: USER_EMAIL,
-        to: resultQuery.email,
+        to: email,
         subject,
         html: htmlTemplate,
         attachments: [
