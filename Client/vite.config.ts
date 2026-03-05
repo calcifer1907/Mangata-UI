cd941fd36632c271ebf620eaccb0538b058ec82a
Carlos Taborda <tabordac2@gmail.com>
2026-03-04 22:08:27 -0500

    add attibute maximumFileSizeToCacheInBytes

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 3000000,
       },
     }),
   ],
